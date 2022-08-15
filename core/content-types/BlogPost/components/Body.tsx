67f6e8c835cdc9c3624a1f281ebec469037454cc
Sean Hickey <sean.hickey@cyberwoven.com>
2022-08-15 18:19:43 -0400

    Syntax highlighting fix

--- core/content-types/BlogPost/components/Body.tsx
+++ core/content-types/BlogPost/components/Body.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import { mu } from '../../../utility/Markup'
+import hljs from 'highlight.js'
 
 export interface IBody {
   content: string
 }
 export default function Body(props: IBody) {
-
+  useEffect(() => {
+    hljs.highlightAll();
+  }, [])
   return (
     <>
       <Container dangerouslySetInnerHTML={mu(props.content)} className='body'>
