0cac2c575d347ce35413fa0ed89fc00aff845b48
muzu <seanhickey4516@gmail.com>
2022-12-14 19:47:17 -0500

    frontend updates

--- core/content-types/HomePage/components/BlogLink/BlogLink.tsx
+++ core/content-types/HomePage/components/BlogLink/BlogLink.tsx
@@ -5,6 +5,6 @@ import styles from './BlogLink.module.scss'
 
 export default function BlogLink(props: IBlogLink) {
   return (
-    <a className={styles.link} href='/blog'>Blog &rarr;</a>
+    <a className={styles.link + ' blog-link'} href='/blog'>Blog &rarr;</a>
   )
 }
